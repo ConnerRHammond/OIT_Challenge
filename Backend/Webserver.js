224c89e931c336afb1087f0152551a3e418fbf57
Conner Hammond <reuelerman@gmail.com>
2024-01-20 12:43:52 -0700

    Add changes to have a funcitonal Webserver without error handling.

--- Backend/Webserver.js
+++ Backend/Webserver.js
@@ -2,25 +2,45 @@ const express = require('express')
 const fetch = require('node-fetch');
 const app = express()
 const port = 3000
+const API_KEY = process.env.API_KEY
 
-
+/*
+    TODO: 
+    - Add error Handling. 
+    - Separate functinoality to separate files. 
+    - Move variables to .env file
+*/
 app.get('/movies', (req, res) => {
-    console.log(req.query.search)
-
     const url = `https://api.themoviedb.org/3/search/movie?query=${req.query.search}&include_adult=false&language=en-US&page=1`;
     const options = {
     method: 'GET',
     headers: {
-      accept: 'application/json',
-      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhYTBmYWI3MWU4YjJmZWM1YjMzYWEyZjdkY2M1Y2E4NCIsInN1YiI6IjY1YWMwZTYwMTU4Yzg1MDBhYzFiNGE0YyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.lEvZZe-OwCM4vkWtQUpOU5EJ1BjphBF451nYNf7-bio'
+        accept: 'application/json',
+        Authorization: `Bearer ${API_KEY}`
     }
-  };
+    };
+    const express_res = res;
     fetch(url, options)  
         .then(res => res.json())
-        .then(json => console.log(json))
+        .then(json => express_res.status(200).send(process_data(json)))
         .catch(err => console.error('error:' + err));
 })
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
+
+
+function process_data(json) {
+    let return_data = [];
+    for (let i = 0; i < 10 && i < json.results.length; i++) {
+        return_data.push({
+            movie_id: json.results[i].id,
+            title: json.results[i].title,
+            poster_image_url: `http://image.tmdb.org/t/p/original${json.results[i].poster_path}`,
+            popularity_summary: `${json.results[i].popularity} out of ${json.results[i].vote_count}`
+        })
+    }
+    return JSON.stringify(return_data);
+}
+
